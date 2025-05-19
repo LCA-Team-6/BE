28bbdad88c7ab1ba2bec6b57d3b1804a41e1ed37
gkwnstjr <147680778+gkwnstjr@users.noreply.github.com>
2025-05-19 17:21:54 +0900

    Add files via upload

--- src/main/java/com/project/mini/user/controller/UserController.java
+++ src/main/java/com/project/mini/user/controller/UserController.java
@@ -1,7 +1,8 @@
 // JavaScript source code
 package com.project.mini.user.controller;
 
-import com.project.mini.user.dto.UserResponseDto;
+import com.project.mini.common.Response;
+import com.project.mini.user.dto.*;
 import com.project.mini.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,28 +17,28 @@ public class UserController {
 
     // READ: 유저 정보 조회 (마이페이지 등)
     @GetMapping
-    public ResponseEntity<UserResponseDto> getUserInfo() {
-        return ResponseEntity.ok(userService.getCurrentUser());
+    public ResponseEntity<Response<UserResponseDto>> getUserInfo() {
+        return ResponseEntity.ok(Response.success("유저 정보 조회 성공", userService.getCurrentUser()));
     }
 
     // UPDATE: 유저 정보 수정 (이름, 이메일 등)
     @PatchMapping
-    public ResponseEntity<String> updateUserInfo(@RequestBody UserUpdateDto updateDto) {
+    public ResponseEntity<Response<Void>> updateUserInfo(@RequestBody UserUpdateDto updateDto) {
         userService.updateUserInfo(updateDto);
-        return ResponseEntity.ok("수정된 값");
+        return ResponseEntity.ok(Response.success("수정된 값", null));
     }
 
     // UPDATE: 비밀번호 변경
     @PostMapping("/password")
-    public ResponseEntity<String> changePassword(@RequestBody ChangePasswordDto dto) {
+    public ResponseEntity<Response<Void>> changePassword(@RequestBody ChangePasswordDto dto) {
         userService.changePassword(dto);
-        return ResponseEntity.ok("ok");
+        return ResponseEntity.ok(Response.success("비밀번호 변경 완료", null));
     }
 
     // DELETE: 유저 삭제
     @DeleteMapping
-    public ResponseEntity<String> deleteUser() {
+    public ResponseEntity<Response<Void>> deleteUser() {
         userService.deleteCurrentUser();
-        return ResponseEntity.ok("삭제된 값");
+        return ResponseEntity.ok(Response.success("삭제된 값", null));
     }
 }
