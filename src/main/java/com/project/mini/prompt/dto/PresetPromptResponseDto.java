4fcc9cde1b6fa77c0605c58bcbbdbf3381ad6d84
gkwnstjr <147680778+gkwnstjr@users.noreply.github.com>
2025-05-21 15:16:35 +0900

    Add files via upload

---
+++ src/main/java/com/project/mini/prompt/dto/PresetPromptResponseDto.java
@@ -0,0 +1,20 @@
+package com.project.mini.preset.prompt.dto;
+
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PresetPromptResponseDto {
+    private Long presetPromptId;
+    private Long userId;
+    private Long toneId;
+    private Long personalityId;
+    private Long styleId;
+    private Long contentId;
+    private String name;
+    private LocalDateTime createdAt;
+}
---
+++ src/main/java/com/project/mini/prompt/repository/PresetPromptRepository.java
@@ -0,0 +1,17 @@
+package com.project.mini.prompt.repository; 
+
+import com.project.mini.prompt.entity.PresetPrompt; 
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
+public interface PresetPromptRepository extends JpaRepository<PresetPrompt, Long> {
+   
+    List<PresetPrompt> findByUserId(Long userId);
+
+
+    Optional<PresetPrompt> findByPresetPromptId(Long presetPromptId);
+
+
+    void deleteByPresetPromptId(Long presetPromptId);
+}
