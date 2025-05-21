700f0f576062aa2658e13cf950e2f40352adab8b
gkwnstjr <147680778+gkwnstjr@users.noreply.github.com>
2025-05-21 14:11:22 +0900

    Delete src/main/java/com/project/mini/prompt directory

--- src/main/java/com/project/mini/prompt/PresetPromptRepository.java
+++
@@ -1,6 +0,0 @@
-package com.project.mini.prompt;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PresetPromptRepository extends JpaRepository<PresetPrompt, Long> {
-}
